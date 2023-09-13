56f6fb2b2e370bbf96e2a432cec9d23242973c90
Dotto-Luis <93018629+Dotto-Luis@users.noreply.github.com>
2023-09-13 20:34:17 -0300

    redefinition of the function

--- Finance/Ecommerce-Latam/src/extract.py
+++ Finance/Ecommerce-Latam/src/extract.py
@@ -1,12 +1,30 @@
 from typing import Dict
-import pandas as pd
 
 import requests
 from pandas import DataFrame, read_csv, read_json, to_datetime
 
-public_holidays_url = 'https://date.nager.at/api/v3/PublicHolidays'
 
 def get_public_holidays(public_holidays_url: str, year: str) -> DataFrame:
+    """Get the public holidays for the given year for Brazil.
+
+    Args:
+        public_holidays_url (str): url to the public holidays.
+        year (str): The year to get the public holidays for.
+
+    Raises:
+        SystemExit: If the request fails.
+
+    Returns:
+        DataFrame: A dataframe with the public holidays.
+    """
+    # TODO: Implement this function.
+    # You must use the requests library to get the public holidays for the given year.
+    # The url is public_holidays_url/{year}/BR.
+    # You must delete the columns "types" and "counties" from the dataframe.
+    # You must convert the "date" column to datetime.
+    # You must raise a SystemExit if the request fails. Research the raise_for_status
+    # method from the requests library.
+    public_holidays_url = 'https://date.nager.at/api/v3/PublicHolidays'
     url = f'{public_holidays_url}/{year}/BR'
     try:
         response = requests.get(url)
@@ -21,21 +39,29 @@ def get_public_holidays(public_holidays_url: str, year: str) -> DataFrame:
         print(f"Error fetching data from API: {e}")
         raise SystemExit
     
-csv_folder = r'C:\Users\n600\Documents\ML-Projects\AnyoneAI-SprintI\dataset'
 
-def extract(csv_folder: str, csv_table_mapping: Dict[str, str], public_holidays_url: str) -> Dict[str, DataFrame]:
-    dataframes = {table_name: read_csv(f"{csv_folder}/{csv_file}")
-        for csv_file, table_name in csv_table_mapping.items()}
-    holidays = get_public_holidays(public_holidays_url, "2016")
+
+def extract(
+    csv_folder: str, csv_table_mapping: Dict[str, str], public_holidays_url: str
+) -> Dict[str, DataFrame]:    
+    """Extract the data from the csv files and load them into the dataframes.
+    Args:
+        csv_folder (str): The path to the csv's folder.
+        csv_table_mapping (Dict[str, str]): The mapping of the csv file names to the
+        table names.
+        public_holidays_url (str): The url to the public holidays.
+    Returns:
+        Dict[str, DataFrame]: A dictionary with keys as the table names and values as
+        the dataframes.
+    """
+    csv_folder = r'C:\Users\n600\Documents\ML-Projects\AnyoneAI-SprintI\dataset'
+    
+    dataframes = {
+        table_name: read_csv(f"{csv_folder}/{csv_file}")
+        for csv_file, table_name in csv_table_mapping.items()
+    }
     holidays = get_public_holidays(public_holidays_url, "2017")
-    holidays = get_public_holidays(public_holidays_url, "2018")
+
     dataframes["public_holidays"] = holidays
 
     return dataframes
-
-
-holidays_2016 = get_public_holidays(public_holidays_url, "2016")
-holidays_2017 = get_public_holidays(public_holidays_url, "2017")
-holidays_2018 = get_public_holidays(public_holidays_url, "2018")
-
-holidaysBR = pd.concat([holidays_2016, holidays_2017, holidays_2018])
