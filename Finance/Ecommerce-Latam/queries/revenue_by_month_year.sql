78a13c463426accff997484cde07bbb604a0bac1
Dotto-Luis <93018629+Dotto-Luis@users.noreply.github.com>
2023-09-21 15:22:29 -0300

    wrong group by

--- Finance/Ecommerce-Latam/queries/revenue_by_month_year.sql
+++ Finance/Ecommerce-Latam/queries/revenue_by_month_year.sql
@@ -6,22 +6,29 @@
 -- Year2018, with the revenue per month of 2018 (0.00 if it doesn't exist).
 
 SELECT 
-	STRFTIME('%Y', oo.order_estimated_delivery_date) AS 'Year',
-	STRFTIME('%m', oo.order_estimated_delivery_date) AS 'month_no',
-	CASE 
-	WHEN STRFTIME('%m', oo.order_estimated_delivery_date) = '01' THEN 'jan'
-    WHEN STRFTIME('%m', oo.order_estimated_delivery_date) = '02' THEN 'feb'
-	WHEN STRFTIME('%m', oo.order_estimated_delivery_date) = '03' THEN 'mar'
-	WHEN STRFTIME('%m', oo.order_estimated_delivery_date) = '04' THEN 'apr' 
-	WHEN STRFTIME('%m', oo.order_estimated_delivery_date) = '05' THEN 'may' 
-	WHEN STRFTIME('%m', oo.order_estimated_delivery_date) = '06' THEN 'jun' 
-	WHEN STRFTIME('%m', oo.order_estimated_delivery_date) = '07' THEN 'jul' 
-	WHEN STRFTIME('%m', oo.order_estimated_delivery_date) = '08' THEN 'aug' 
-	WHEN STRFTIME('%m', oo.order_estimated_delivery_date) = '09' THEN 'sep' 
-	WHEN STRFTIME('%m', oo.order_estimated_delivery_date) = '10' THEN 'oct' 
-	WHEN STRFTIME('%m', oo.order_estimated_delivery_date) = '11' THEN 'nov'
-	WHEN STRFTIME('%m', oo.order_estimated_delivery_date) = '12' THEN 'dec' END AS 'month',
-	SUM(oop.payment_value) AS Revenue
+    STRFTIME('%m', oo.order_estimated_delivery_date) AS 'month_no',
+    CASE STRFTIME('%m', oo.order_delivered_customer_date) 
+        WHEN '01' THEN 'Jan'
+        WHEN '02' THEN 'Feb'
+        WHEN '03' THEN 'Mar'
+        WHEN '04' THEN 'Apr' 
+        WHEN '05' THEN 'May' 
+        WHEN '06' THEN 'Jun' 
+        WHEN '07' THEN 'Jul' 
+        WHEN '08' THEN 'Aug' 
+        WHEN '09' THEN 'Sep' 
+        WHEN '10' THEN 'Oct' 
+        WHEN '11' THEN 'Nov'
+        WHEN '12' THEN 'Dec' END AS 'month',
+    SUM(CASE WHEN STRFTIME('%Y', oo.order_delivered_customer_date) = '2016' THEN oop.payment ELSE 0 END) AS Year2016,
+    SUM(CASE WHEN STRFTIME('%Y', oo.order_delivered_customer_date) = '2017' THEN oop.payment ELSE 0 END) AS Year2017,
+    SUM(CASE WHEN STRFTIME('%Y', oo.order_delivered_customer_date) = '2018' THEN oop.payment ELSE 0 END) AS Year2018
 FROM olist_orders oo
-JOIN olist_order_payments oop ON oo.order_id = oop.order_id 
-GROUP BY Year, month_no
+INNER JOIN (SELECT DISTINCT order_id, MIN(payment_value) AS payment 
+	FROM olist_order_payments 
+	GROUP BY order_id)
+	AS oop ON oo.order_id = oop.order_id
+WHERE month_no IS NOT NULL AND oo.order_delivered_customer_date IS NOT NULL AND oo.order_status = 'delivered'
+--JOIN olist_order_payments oop ON oo.order_id = oop.order_id
+GROUP BY month
+ORDER BY month_no ASC;
